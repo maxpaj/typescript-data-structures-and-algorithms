1bcb6f568f59057e30f910ef4d2c51fe89ae2564
Maximilian Paju <maximilian.paju@gmail.com>
2019-06-08 01:08:08 +0200

    fix: heapsort test

--- heap/heapsort-test.ts
+++ heap/heapsort-test.ts
@@ -1,4 +1,20 @@
 import { heapsort } from "./heapsort";
 
-const sorted = heapsort([0, 1, -4, 9, 6, -3]);
-console.log(sorted.join(","));
+function randomArray(length: number, max: number) {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(Math.round(Math.random() * max));
+  }
+  return arr;
+}
+
+function run(size: number) {
+  const arr = randomArray(size, Number.MAX_SAFE_INTEGER);
+  const start = process.hrtime();
+  heapsort(arr);
+  console.log(process.hrtime(start));
+}
+
+run(1000000);
+run(10000000);
+run(100000000);
