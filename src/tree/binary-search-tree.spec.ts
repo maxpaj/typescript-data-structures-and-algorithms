06495d88f97071fe2830313bcd7ce2b89708408b
Maximilian Paju <maximilian.paju@gmail.com>
2019-12-28 15:09:04 +0100

    bst tests

---
+++ src/tree/binary-search-tree.spec.ts
@@ -0,0 +1,60 @@
+import { BinarySearchTree } from "./binary-search-tree";
+import { ComparableNumber } from "../utility/comparable";
+import { expect } from "chai";
+
+describe("BinarySearchTree", () => {
+  describe("search", () => {
+    it("should return the node given by the key", () => {
+      const bst = new BinarySearchTree<ComparableNumber, string>(
+        new ComparableNumber(0),
+        "ABC"
+      );
+      bst.insert(new ComparableNumber(1), "DEF");
+      bst.insert(new ComparableNumber(3), "GHI");
+      bst.insert(new ComparableNumber(-2), "JKL");
+
+      const found = bst.search(new ComparableNumber(-2));
+
+      expect(found).to.equal("JKL");
+    });
+  });
+  
+  describe("remove", () => {
+    it("should remove the node given by the key", () => {
+      const bst = new BinarySearchTree<ComparableNumber, string>(
+        new ComparableNumber(0),
+        "ABC"
+      );
+      bst.insert(new ComparableNumber(1), "DEF");
+      bst.insert(new ComparableNumber(3), "GHI");
+      bst.insert(new ComparableNumber(-2), "JKL");
+      bst.remove(new ComparableNumber(1));
+
+      const found = bst.search(new ComparableNumber(1));
+
+      expect(found).to.equal(null);
+    });
+  });
+
+  describe("inorder", () => {
+    it("should iterate the tree in the correct order", () => {
+      const bst = new BinarySearchTree<ComparableNumber, string>(
+        new ComparableNumber(0),
+        "ABC"
+      );
+      bst.insert(new ComparableNumber(1), "DEF");
+      bst.insert(new ComparableNumber(3), "GHI");
+      bst.insert(new ComparableNumber(-2), "JKL");
+
+      const order = [];
+      bst.inorder((n: BinarySearchTree<ComparableNumber, string>) => {
+        order.push(n.value);
+      });
+
+      expect(order[0]).to.equal("JKL");
+      expect(order[1]).to.equal("ABC");
+      expect(order[2]).to.equal("DEF");
+      expect(order[3]).to.equal("GHI");
+    });
+  });
+});
--- src/tree/binary-search-tree.ts
+++ src/tree/binary-search-tree.ts
@@ -1,53 +1,72 @@
 import { Comparable } from "../utility/comparable";
 
-export class BinarySearchTree<T extends Comparable<T>> {
-  leftChild: BinarySearchTree<T> = null;
-  rightChild: BinarySearchTree<T> = null;
-  data: T;
+export class BinarySearchTree<K extends Comparable<K>, V> {
+  private leftChild: BinarySearchTree<K, V> = null;
+  private rightChild: BinarySearchTree<K, V> = null;
+  private key: K;
+  public value: V;
 
-  constructor(data: T) {
-    this.data = data;
+  constructor(key: K, value: V) {
+    this.key = key;
+    this.value = value;
   }
 
-  insert(node: BinarySearchTree<T>) {
-    if (node.data.lesserThan(this.data)) {
+  insert(key: K, value: V) {
+    if (key.lesserThan(this.key)) {
       if (this.leftChild === null) {
-        this.leftChild = node;
+        this.leftChild = new BinarySearchTree<K, V>(key, value);
       } else {
-        this.leftChild.insert(node);
+        this.leftChild.insert(key, value);
       }
-    } else {
+    } else if(key.greaterThan(this.key)) {
       if (this.rightChild === null) {
-        this.rightChild = node;
+        this.rightChild = new BinarySearchTree<K, V>(key, value);
       } else {
-        this.rightChild.insert(node);
+        this.rightChild.insert(key, value);
       }
     }
   }
 
-  search(data: T) {
-    if (this.data.equals(data)) {
-      return this;
+  remove(key: K) {
+    if (this.leftChild !== null && key.lesserThan(this.key)) {
+      if (this.leftChild.key.equals(key)) {
+        this.leftChild = null;
+      } else {
+        this.leftChild.remove(key);
+      }
+    } else if (this.rightChild !== null && key.greaterThan(this.key)) {
+      if (this.rightChild.key.equals(key)) {
+        this.rightChild = null;
+      } else {
+        this.rightChild.remove(key);
+      }
+    }
+  }
+
+  search(key: K) {
+    if (this.key.equals(key)) {
+      return this.value;
     }
 
-    if (this.data.greaterThan(data) && this.leftChild) {
-      return this.leftChild.search(data);
-    } else if (this.data.lesserThan(data) && this.rightChild) {
-      return this.rightChild.search(data);
+    if (this.key.greaterThan(key) && this.leftChild) {
+      return this.leftChild.search(key);
+    } else if (this.key.lesserThan(key) && this.rightChild) {
+      return this.rightChild.search(key);
     }
     return null;
   }
 
-  inorder() {
+  inorder(f: (node: BinarySearchTree<K, V>) => void) {
     if (this.leftChild) {
-      this.leftChild.inorder();
+      this.leftChild.inorder(f);
     }
+    f(this);
     if (this.rightChild) {
-      this.rightChild.inorder();
+      this.rightChild.inorder(f);
     }
   }
-
+  
   toString() {
-    return "" + this.data;
+    return "" + this.key;
   }
 }
