29a60e971bfb535fdd7c1f800b4d7b14d426a150
Maximilian Paju <maximilian.paju@gmail.com>
2020-01-01 15:39:02 +0100

    added duplicates test

--- src/algorithm/binarysearch/binarysearch.spec.ts
+++ src/algorithm/binarysearch/binarysearch.spec.ts
@@ -6,18 +6,40 @@ describe("binarysearch", () => {
     it("should return the index of the found element", () => {
         const arr = [
             new ComparableNumber(1),
+            new ComparableNumber(3),
             new ComparableNumber(4),
             new ComparableNumber(8),
             new ComparableNumber(10),
             new ComparableNumber(19),
             new ComparableNumber(55),
+            new ComparableNumber(150),
             new ComparableNumber(200),
             new ComparableNumber(1882),
             new ComparableNumber(1889),
             new ComparableNumber(1890)
         ];
 
-        expect(binarysearch(arr, new ComparableNumber(4))).to.equal(1);
-        expect(binarysearch(arr, new ComparableNumber(200))).to.equal(6);
+        expect(binarysearch(arr, new ComparableNumber(4))).to.equal(2);
+        expect(binarysearch(arr, new ComparableNumber(200))).to.equal(8);
+    });
+
+    it("should return the highest index of a found element if there are duplicates of the found element", () => {
+        const arr = [
+            new ComparableNumber(1),
+            new ComparableNumber(4),
+            new ComparableNumber(4),
+            new ComparableNumber(8),
+            new ComparableNumber(10),
+            new ComparableNumber(19),
+            new ComparableNumber(55),
+            new ComparableNumber(200),
+            new ComparableNumber(200),
+            new ComparableNumber(1882),
+            new ComparableNumber(1889),
+            new ComparableNumber(1890)
+        ];
+
+        expect(binarysearch(arr, new ComparableNumber(4))).to.equal(2);
+        expect(binarysearch(arr, new ComparableNumber(200))).to.equal(8);
     });
 });
