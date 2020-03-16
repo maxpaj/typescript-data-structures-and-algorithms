22e69b17f8adf8641161ac984cae8f81a630252c
Maximilian Paju <maximilian.paju@gmail.com>
2020-03-16 16:49:25 +0100

    reverse hi lo

--- src/algorithm/reverse/reverse.spec.ts
+++ src/algorithm/reverse/reverse.spec.ts
@@ -7,4 +7,10 @@ describe("reverse", () => {
         expect(reverse([1, 2, 3, 4])).to.eql([4, 3, 2, 1]);
         expect(reverse([1, 2, 3, 4, 5])).to.eql([5, 4, 3, 2, 1]);
     });
+
+    it("should reverse an array given two indices", () => {
+        expect(reverse([1], 0, 0)).to.eql([1]);
+        expect(reverse([1, 2], 0, 1)).to.eql([2, 1]);
+        expect(reverse([1, 2, 3, 4, 5], 1, 4)).to.eql([1, 5, 4, 3, 2]);
+    });
 });
