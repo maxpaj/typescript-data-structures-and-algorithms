7f5db725a55c7a26263580a3e5a4ef5d0714f781
Maximilian Paju <maximilian.paju@gmail.com>
2022-05-10 23:03:24 +0200

    test: added test for different sizes of arrays

--- src/algorithms/zip/zip.spec.ts
+++ src/algorithms/zip/zip.spec.ts
@@ -2,7 +2,7 @@ import { zip } from "./zip";
 import { expect } from "chai";
 
 describe("zip", () => {
-    it("should transform all elements of an array", () => {
+    it("should zip together elements of two arrays into a single array", () => {
         const arr1 = [1, 2, 3, 4];
         const arr2 = ["a", "b", "c", "d"];
         const zipped = zip(arr1, arr2);
@@ -10,5 +10,15 @@ describe("zip", () => {
         expect(zipped[1]).to.eql([2, "b"]);
         expect(zipped[2]).to.eql([3, "c"]);
         expect(zipped[3]).to.eql([4, "d"]);
+    });
+
+    it("should work with different sizes of arrays and fill the remaining values with undefined", () => {
+        const arr1 = [1, 2, 3, 4];
+        const arr2 = ["a", "b", "c"];
+        const zipped = zip(arr1, arr2);
+        expect(zipped[0]).to.eql([1, "a"]);
+        expect(zipped[1]).to.eql([2, "b"]);
+        expect(zipped[2]).to.eql([3, "c"]);
+        expect(zipped[3]).to.eql([4, undefined]);
     });
 });
